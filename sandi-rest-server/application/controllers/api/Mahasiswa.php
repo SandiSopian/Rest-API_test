ae08d8f93adde27570dcc0892979745663814b79
Sandi Sopian <shunasand73@gmail.com>
2021-12-03 13:05:51 +0700

    Membuat Rest Server

--- sandi-rest-server/application/controllers/api/Mahasiswa.php
+++ sandi-rest-server/application/controllers/api/Mahasiswa.php
@@ -16,13 +16,101 @@ class Mahasiswa extends REST_Controller
     
     public function index_get() 
     {
-        $mahasiswa = $this->mahasiswa->getMahasiswa();
+        $id = $this->get('id');
+        if ($id === null) {
+            $mahasiswa = $this->mahasiswa->getMahasiswa();
+        } else {
+            $mahasiswa = $this->mahasiswa->getMahasiswa($id);
+
+        }
+       
 
         if ($mahasiswa) {
             $this->response([
                 'status' => true,
                 'data' => $mahasiswa
             ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'data' => 'id not found'
+            ], REST_Controller::HTTP_NOT_FOUND);
+
         }
     }
+
+    public function index_delete()
+    {
+        $id = $this->delete('id');
+
+        if ($id === null) {
+            $this->response([
+                'status' => false,
+                'data' => 'provide an id'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            if( $this->mahasiswa->deleteMahasiswa($id) > 0 ) {
+                // ok
+                $this->response([
+                    'status' => true,
+                    'data' => $id,
+                    'message' => 'deleted!'
+                ], REST_Controller::HTTP_NO_CONTENT);
+            } else {
+                
+            }
+        }
+
+    }
+
+
+    public function index_post()
+    {
+        $data = [
+            'nrp' => $this->post('nrp'),
+            'nama' => $this->post('nama'),
+            'email' => $this->post('email'),
+            'jurusan' => $this->post('jurusan'),
+        ];
+
+        if ($this->mahasiswa->createMahasiswa($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new mahasiswa has been created'
+        ], REST_Controller::HTTP_CREATED);
+        } else {
+            $this->response([
+                'status' => false,
+                'data' => 'failed to create new data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+
+    public function index_put()
+    {
+        $id = $this->put('id');
+        $data = [
+            'nrp' => $this->put('nrp'),
+            'nama' => $this->put('nama'),
+            'email' => $this->put('email'),
+            'jurusan' => $this->put('jurusan'),
+        ];
+
+        if ($this->mahasiswa->updateMahasiswa($data, $id) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new mahasiswa has been modified!'
+        ], REST_Controller::HTTP_NO_CONTENT);
+        } else {
+            $this->response([
+                'status' => false,
+                'data' => 'failed to modified new data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+
+
+
 }
